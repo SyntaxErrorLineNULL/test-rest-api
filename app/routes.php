52640c37f23ed8a3d8c6fcc23b6f88be92c067b3
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-03-25 23:08:34 +0300

    init project

---
+++ app/routes.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+    $app->post('/data', function (Request $request, Response $response) {
+        $parameters = json_decode((array)$request->getParsedBody(), TRUE);
+        $email = $parameters['email'];
+
+        $response->getBody()->write("Hello $email!");
+        return $response;
+    });
+
+
+
+    $app->post('/name', function (Request $request, Response $response, $args): Response {
+        $data = $request->getBody()->getContents();
+
+        $html = var_export($data, true);
+
+        $response->getBody()->write($html);
+
+        return $response;
+    });
+
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->group('/sign_id', function (Group $group) {
+        $group->post('/data', \App\Application\Handlers\SignIn\SignInHandler::class);
+    });
+};
