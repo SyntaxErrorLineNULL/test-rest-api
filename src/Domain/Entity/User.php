a2f36a121e6b111d2108c90d873d004ddf0aa5d2
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-03-26 12:45:24 +0300

    #8 user entity

---
+++ src/Domain/Entity/User.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer",nullable=false)
+     * @var int
+     */
+    public int $id;
+
+    /**
+     * @ORM\Column(type="string",length=50)
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @ORM\Column(type="string",nullable=false,length=32,options={"fixed"=true},unique=true)
+     * @var string
+     */
+    public string $email;
+
+    /**
+     * @ORM\Column(type="text",nullable=false)
+     * @var string
+     */
+    public string $password_hash;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    public bool $activated;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     * @var DateTimeImmutable|null
+     */
+    public ?DateTimeImmutable $createdAt;
+
+    /**
+     * User constructor.
+     * @param string $email
+     * @param string|null $name
+     * @param string $password
+     */
+    public function __construct(string $email, ?string $name, string $password)
+    {
+        $this->email = $email;
+        $this->name = $name;
+        $this->password_hash = $password;
+        $this->activated = false;
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function isActive(): bool
+    {
+        return $this->activated = true;
+    }
+}
