4a9cd581d883b0907ef561b695844316574f396e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-03-26 15:17:33 +0300

    #9 token entity

---
+++ src/Domain/Entity/ConfirmationToken.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DoctrineUserRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="confirmation_token")
+ */
+class ConfirmationToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer",nullable=false)
+     * @var int
+     */
+    public int $id;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    public string $value;
+
+    /**
+     * @ORM\Column(type="datetime_immutable",nullable=true)
+     * @var DateTimeImmutable|null
+     */
+    public ?DateTimeImmutable $createAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @var User
+     */
+    public User $userId;
+
+    /**
+     * ConfirmationToken constructor.
+     * @param string $value
+     * @param User $userId
+     */
+    public function __construct(string $value, User $userId)
+    {
+        $this->value = $value;
+        $this->userId = $userId;
+
+        $this->createAt = new DateTimeImmutable();
+    }
+
+
+
+}
