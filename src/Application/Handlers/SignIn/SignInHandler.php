e7cd5d2b7d797f233a64e2e3afae89a9c5040bdb
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-03-29 15:00:04 +0300

    #7 jms serialize

--- src/Application/Handlers/SignIn/SignInHandler.php
+++ src/Application/Handlers/SignIn/SignInHandler.php
@@ -1,9 +1,12 @@
 <?php
 
-
+/**
+ * https://insights.project-a.com/serializing-data-in-php-a-simple-primer-on-the-jms-serializer-and-fos-rest-f469d7d5b902
+ */
 namespace App\Application\Handlers\SignIn;
 
 
+use App\Core\Service\RequestData;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -12,12 +15,25 @@ use Psr\Http\Server\RequestHandlerInterface;
 class SignInHandler implements RequestHandlerInterface
 {
 
+    private RequestData $data;
+
+    /**
+     * SignInHandler constructor.
+     * @param RequestData $data
+     */
+    public function __construct(RequestData $data)
+    {
+        $this->data = $data;
+    }
+
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $data = $request->getBody();
+        /** @var SignInRequest $data */
+        $data = $this->data->getData($request, SignInRequest::class);
 
-        $body = json_decode($data, true);
+        $email = $data->email;
 
-        return new JsonResponse([$body]);
+        return new JsonResponse($email);
     }
 }
---
+++ src/Core/Service/RequestData.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Core\Service;
+
+
+use JMS\Serializer\SerializerBuilder;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RequestData
+{
+
+    private SerializerBuilder $serializerBuilder;
+
+    /**
+     * RequestData constructor.
+     * @param SerializerBuilder $serializerBuilder
+     */
+    public function __construct(SerializerBuilder $serializerBuilder)
+    {
+        $this->serializerBuilder = $serializerBuilder;
+    }
+
+
+    public function getData(ServerRequestInterface $request, string $object): object
+    {
+        $serializer = $this->serializerBuilder->build();
+        return $serializer->deserialize((string)$request->getBody(), $object, 'json');
+    }
+}
