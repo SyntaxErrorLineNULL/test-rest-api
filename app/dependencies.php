9c13736603a0f1148064359475551984a5cca3cb
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-06 19:18:07 +0300

    container fix

--- app/dependencies.php
+++ app/dependencies.php
@@ -5,6 +5,7 @@ use App\Application\Domain\Entities\ConfirmationToken;
 use App\Application\Domain\Repository\UserRepository;
 use App\Application\Infrastructure\Repository\DoctrineConfirmationToken;
 use App\Application\Infrastructure\Repository\DoctrineUserRepository;
+use App\Core\Service\Container;
 use Doctrine\ORM\EntityManager;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -14,13 +15,14 @@ use Psr\Log\LoggerInterface;
 use Slim\App;
 use Slim\Factory\AppFactory;
 
-return [
-    App::class => function(ContainerInterface $container) {
+return function (Container $container) : void
+{
+    $container[App::class] = function(ContainerInterface $container) {
         AppFactory::setContainer($container);
         return AppFactory::create();
-    },
+    };
 
-    LoggerInterface::class => function (ContainerInterface $container) {
+    $container[LoggerInterface::class] = function (Container $container) {
         $settings = $container->get('settings');
         $loggerSettings = $settings->get('logger');
         $logger = new Logger($loggerSettings['name']);
@@ -31,15 +33,15 @@ return [
         $logger->pushHandler(new StreamHandler($loggerSettings['path'], $loggerSettings['level']));
 
         return $logger;
-    },
+    };
 
-    UserRepository::class => function (ContainerInterface $container) {
+    $container[UserRepository::class] = function (Container $container) {
         $em = $container->get(EntityManager::class);
         return new DoctrineUserRepository($em);
-    },
+    };
 
-    ConfirmationToken::class => function (ContainerInterface $container) {
+    $container[ConfirmationToken::class] = function (Container $container) {
         $em = $container->get(EntityManager::class);
         return new DoctrineConfirmationToken($em);
-    }
-];
+    };
+};
