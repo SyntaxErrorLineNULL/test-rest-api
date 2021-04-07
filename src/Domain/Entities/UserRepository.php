9eca60586deae8c116faefff59455d56e12d74c7
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-08 00:04:05 +0300

    add user entity repository

---
+++ src/Domain/Entities/UserRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Entities;
+
+
+interface UserRepository
+{
+    /**
+     * @param User $user
+     */
+    public function add(User $user): void;
+
+    /**
+     * @param User $user
+     */
+    public function remove(User $user): void;
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    public function getById(int $id): User;
+
+    /**
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User;
+
+    /**
+     * @param string $email
+     * @param string $password
+     * @return User|null
+     */
+    public function findByEmailAndPassword(string $email, string $password): ?User;
+}
