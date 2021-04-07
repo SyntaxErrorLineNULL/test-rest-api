a7a8ef3de07ac7f48e7f00cfb1de4ea0c75c27b7
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-08 00:02:27 +0300

    add user entity

---
+++ src/Domain/Entities/User.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Entities;
+
+
+use App\Domain\Service\PasswordService;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DoctrineUserRepository::class)
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer",nullable=false)
+     * @var int
+     */
+    public int $id;
+
+    /**
+     * @ORM\Column(type="string",length=50)
+     * @var string|null
+     */
+    public ?string $name;
+
+    /**
+     * @ORM\Column(type="string",nullable=false,length=32,options={"fixed"=true},unique=true)
+     * @var string
+     */
+    public string $email;
+
+    /**
+     * @ORM\Column(type="text",nullable=false)
+     * @var string
+     */
+    public string $password_hash;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    public bool $activated;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     * @var DateTimeImmutable|null
+     */
+    public ?DateTimeImmutable $createdAt;
+
+    /**
+     * User constructor.
+     * @param string $email
+     * @param string|null $name
+     * @param string $password
+     * @param PasswordService $passwordService
+     */
+    public function __construct(string $email, ?string $name, string $password, PasswordService $passwordService)
+    {
+        $this->email = $email;
+        $this->name = $name;
+        $this->password_hash = $passwordService->hash($password);
+        $this->activated = false;
+    }
+
+    public function isActive(): void
+    {
+        $this->activated = true;
+        $this->createdAt = new DateTimeImmutable();
+    }
+}
