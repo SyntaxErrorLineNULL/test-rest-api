78fcf93668b1ea65d156df0215d971e29aeb73d6
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-08 10:42:36 +0300

    doctrine settings

--- app/settings.php
+++ app/settings.php
@@ -14,5 +14,23 @@ return function (ContainerBuilder $containerBuilder) {
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => Logger::DEBUG,
         ],
+
+        'doctrine' => [
+            'auto_generate_proxies' => true,
+            'cache' => null,
+            'cache_dir' => __DIR__ . '/../data/cache/doctrine/cache',
+            'proxy_dir' => __DIR__ . '/../data/cache/proxies',
+            'connection' => [
+                'driver' => 'pdo_pgsql',
+                'host' => '127.0.0.1',
+                'dbname' => 'app',
+                'user' => 'postgres',
+                'password' => '12345',
+                'charset' => 'utf-8'
+            ],
+            'metadata_dirs' => [
+                __DIR__ . '/../src/Domain/Entities'
+            ],
+        ],
     ]);
 };
