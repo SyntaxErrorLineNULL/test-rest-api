5631ec4d0f03c9e69cba8ea79e41fc4df323cb1f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-08 11:02:46 +0300

    carryover exception*

--- src/Application/Domain/DomainException/DomainRecordNotFoundException.php
+++ src/Application/Domain/DomainException/DomainRecordNotFoundException.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Domain\DomainException;
+namespace App\Application\Domain\DomainException;
 
 class DomainRecordNotFoundException extends DomainException
 {
