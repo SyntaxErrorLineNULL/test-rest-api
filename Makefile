4c706511d39d55829a7ef5674468f8a158136fa9
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-08 11:52:16 +0300

    add makefile, fix repository container

---
+++ Makefile
@@ -0,0 +1,5 @@
+init: down up
+up:
+	docker-compose -f docker-compose.yml up -d
+down:
+	docker-compose -f docker-compose.yml down -v --remove-orphans
--- app/repositories.php
+++ app/repositories.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-use App\Domain\User\UserRepository;
-use App\Infrastructure\Persistence\User\InMemoryUserRepository;
+use App\Application\Domain\User\UserRepository;
+use App\Application\Infrastructure\Persistence\User\InMemoryUserRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
