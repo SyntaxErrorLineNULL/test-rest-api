e95eca6ee98af4c4781fcfd5a680f66dc8980539
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-08 11:06:42 +0300

    carryover entity and repository

--- src/Application/Domain/Entities/UserRepository.php
+++ src/Application/Domain/Entities/UserRepository.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace App\Domain\Entities;
+namespace App\Application\Domain\Entities;
 
 
 interface UserRepository
--- src/Application/Domain/User/UserRepository.php
+++ src/Application/Domain/User/UserRepository.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Domain\User;
+namespace App\Application\Domain\User;
 
 interface UserRepository
 {
