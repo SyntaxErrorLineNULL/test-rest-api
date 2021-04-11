10a89dc8500fec001594b7a2db35d98c8d055839
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-12 00:45:16 +0300

    add logger

--- app/settings.php
+++ app/settings.php
@@ -7,12 +7,20 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Monolog\Logger;
 
 return function (ContainerBuilder $containerBuilder) {
 
     // Global Settings Object
     $containerBuilder->addDefinitions([
         'settings' => [
+            // Logger metadata
+            'logger' => [
+                'name' => 'rest-api',
+                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                'level' => Logger::DEBUG,
+            ],
+
             'doctrine' => [
                 // if true, metadata caching is forcefully disabled
                 'dev_mode' => true,
