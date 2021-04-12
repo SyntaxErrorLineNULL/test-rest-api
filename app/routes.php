5c58705b57b9e3200ec2f0442971f234690ca65c
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-12 23:42:44 +0300

    add nginx config, docker services

--- app/routes.php
+++ app/routes.php
@@ -7,10 +7,18 @@
 declare(strict_types=1);
 
 use App\Api\Action\SignUp\SignUpHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
+
+    $app->post('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
     $app->group('/api', function (Group $group) {
         $group->post('/sign_up', SignUpHandler::class);
     });
