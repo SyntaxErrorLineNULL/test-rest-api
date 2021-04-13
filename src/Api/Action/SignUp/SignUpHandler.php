763b37e9e96265797d2ffdd1f97967b97c34cf98
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-13 18:06:44 +0300

    request schema

--- src/Api/Action/SignUp/SignUpHandler.php
+++ src/Api/Action/SignUp/SignUpHandler.php
@@ -10,7 +10,10 @@ namespace App\Api\Action\SignUp;
 
 
 use App\Application\Domain\Repository\UserRepository;
+use App\Core\Service\PasswordService;
+use App\Core\Service\RequestSchema;
 use Laminas\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -18,18 +21,27 @@ use Psr\Http\Server\RequestHandlerInterface;
 class SignUpHandler implements RequestHandlerInterface
 {
     private UserRepository $userRepository;
+    private PasswordService $passwordService;
+    private RequestSchema $schema;
 
     /**
      * SignUpHandler constructor.
      * @param UserRepository $userRepository
+     * @param PasswordService $passwordService
+     * @param RequestSchema $schema
      */
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, PasswordService $passwordService, RequestSchema $schema)
     {
         $this->userRepository = $userRepository;
+        $this->passwordService = $passwordService;
+        $this->schema = $schema;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new EmptyResponse(201);
+        /** @var SignUpSchema $requestSchema */
+        $requestSchema = $this->schema->deserializeBySchema($request, SignUpSchema::class);
+
+        return new JsonResponse($requestSchema->password);
     }
 }
---
+++ src/Api/Action/SignUp/SignUpSchema.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Api\Action\SignUp;
+
+class SignUpSchema
+{
+    /**
+     * @var string
+     */
+    public string $email;
+
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var string
+     */
+    public string $password;
+}
