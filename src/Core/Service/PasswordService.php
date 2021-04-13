dca354647a47687b0c8999c4cab3dfdde39effbf
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-13 16:50:20 +0300

    create password services

---
+++ src/Core/Service/PasswordService.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Core\Service;
+
+
+use Webmozart\Assert\Assert;
+
+use RuntimeException;
+
+class PasswordService
+{
+    private int $value;
+
+    /**
+     * PasswordService constructor.
+     * @param int $value
+     */
+    public function __construct(int $value = PASSWORD_ARGON2_DEFAULT_MEMORY_COST)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param string $password
+     * @return string
+     */
+    public function hash(string $password): string
+    {
+        Assert::notEmpty($password);
+
+        $hash = password_hash($password, PASSWORD_ARGON2ID, [
+            'value' => $this->value
+        ]);
+
+        if ($hash === null || $hash === false) {
+            throw new RuntimeException('Invalid hash algorithm');
+        }
+
+        return $hash;
+    }
+
+    /**
+     * @param string $password
+     * @param string $hash
+     * @return bool
+     */
+    public function validate(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
+}
