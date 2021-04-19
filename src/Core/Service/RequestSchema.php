96245874c25cbee2efc549ab848b6787d517c460
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-19 23:38:31 +0300

    add link resource

--- src/Core/Service/RequestSchema.php
+++ src/Core/Service/RequestSchema.php
@@ -12,6 +12,7 @@ namespace App\Core\Service;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
@@ -33,7 +34,9 @@ class RequestSchema
                 new AnnotationReader()
             )
         );
-        $objectNormalizer = new ObjectNormalizer($classMetaDataFactory, null, null, new PhpDocExtractor());
+        # https://symfony.com.ua/doc/current/components/property_info.html
+        # https://stackoverflow.com/questions/40033732/denormalize-nested-structure-in-objects-with-symfony-2-serializer
+        $objectNormalizer = new ObjectNormalizer($classMetaDataFactory, null, null, new ReflectionExtractor());
         $this->serializer = new Serializer([
             new ArrayDenormalizer(),
             $objectNormalizer,
