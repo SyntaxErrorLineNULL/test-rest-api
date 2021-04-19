39f715b66d9d3cbe0bf66800df5952c7fefeecbd
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-20 00:26:08 +0300

    #13 create email validate

--- src/Api/Action/SignUp/SignUpSchema.php
+++ src/Api/Action/SignUp/SignUpSchema.php
@@ -8,9 +8,14 @@ declare(strict_types=1);
 
 namespace App\Api\Action\SignUp;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class SignUpSchema
 {
     /**
+     * @Assert\NotBlank
+     * @Assert\Email(message="Данный email '{{ value }}' является неверным",normalizer="strtolower")
+     * @OA\Property
      * @var string
      */
     public string $email;
