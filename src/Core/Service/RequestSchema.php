b55afa7c0cfb8aa24828758636ed4675150961f0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-19 23:27:05 +0300

    fix serialize schema

--- src/Core/Service/RequestSchema.php
+++ src/Core/Service/RequestSchema.php
@@ -9,8 +9,13 @@ declare(strict_types=1);
 namespace App\Core\Service;
 
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
@@ -20,15 +25,16 @@ class RequestSchema
 
     /**
      * RequestSchema constructor.
-     * @param Serializer $serializer
      */
-    public function __construct(Serializer $serializer)
+    public function __construct()
     {
-        $this->serializer = $serializer;
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer(null, null, null, new PhpDocExtractor()), new ArrayDenormalizer()];
+        $this->serializer = new Serializer($normalizers, $encoders);
     }
 
     public function deserializeBySchema(ServerRequestInterface $request, string $schema) {
-        return $this->serializer->deserialize($request, $schema, 'json');
+        return $this->serializer->deserialize($request->getBody(), $schema, 'json');
     }
 
     private function validate() {
