98cc5eb06b77307ee6f951b70fe97509bb26c41e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-20 15:59:40 +0300

    create exception

---
+++ src/Application/Domain/DomainException/DomainNotEmptyEmailException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Application\Domain\DomainException;
+
+
+class DomainNotEmptyEmailException extends \Exception
+{
+
+}
