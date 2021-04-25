eecf4237efea8d0c53315c1461384e310c226407
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-25 16:27:50 +0300

    create token

--- src/Api/Action/SignUp/SignUpHandler.php
+++ src/Api/Action/SignUp/SignUpHandler.php
@@ -9,20 +9,27 @@ declare(strict_types=1);
 namespace App\Api\Action\SignUp;
 
 
+use App\Api\Action\Other\Token;
 use App\Application\Domain\DomainException\DomainNotEmptyEmailException;
+use App\Application\Domain\Entity\ConfirmationToken;
 use App\Application\Domain\Entity\User;
+use App\Application\Domain\Repository\ConfirmationTokenRepository;
 use App\Application\Domain\Repository\UserRepository;
 use App\Application\Infrastructure\DoctrineFlusher;
+use App\Core\Service\JWTService;
 use App\Core\Service\PasswordService;
 use App\Core\Service\RequestSchema;
 use Laminas\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Ramsey\Uuid\Uuid;
 
 class SignUpHandler implements RequestHandlerInterface
 {
     private UserRepository $userRepository;
+    private ConfirmationTokenRepository $confirmationTokenRepository;
     private PasswordService $passwordService;
     private DoctrineFlusher $flusher;
     private RequestSchema $schema;
@@ -30,13 +37,15 @@ class SignUpHandler implements RequestHandlerInterface
     /**
      * SignUpHandler constructor.
      * @param UserRepository $userRepository
+     * @param ConfirmationTokenRepository $confirmationTokenRepository
      * @param PasswordService $passwordService
      * @param DoctrineFlusher $flusher
      * @param RequestSchema $schema
      */
-    public function __construct(UserRepository $userRepository, PasswordService $passwordService, DoctrineFlusher $flusher, RequestSchema $schema)
+    public function __construct(UserRepository $userRepository, ConfirmationTokenRepository $confirmationTokenRepository, PasswordService $passwordService, DoctrineFlusher $flusher, RequestSchema $schema)
     {
         $this->userRepository = $userRepository;
+        $this->confirmationTokenRepository = $confirmationTokenRepository;
         $this->passwordService = $passwordService;
         $this->flusher = $flusher;
         $this->schema = $schema;
@@ -59,10 +68,14 @@ class SignUpHandler implements RequestHandlerInterface
         }
 
         $user = new User($requestSchema->email, $requestSchema->name, $requestSchema->password, $this->passwordService);
+        $token = Uuid::uuid4()->toString();
+        $confirmationToken = new ConfirmationToken($token, $user);
 
         $this->userRepository->add($user);
+        $this->confirmationTokenRepository->add($confirmationToken);
+
         $this->flusher->flush();
 
-        return new EmptyResponse(201);
+        return new JsonResponse(new Token($token), 201);
     }
 }
