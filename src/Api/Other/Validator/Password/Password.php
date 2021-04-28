9bdff492a612fd01d4459286d5360d7cd92cb31e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-04-28 14:44:36 +0300

    add dock block

--- src/Api/Other/Validator/Password/Password.php
+++ src/Api/Other/Validator/Password/Password.php
@@ -8,9 +8,11 @@ declare(strict_types=1);
 
 namespace App\Api\Other\Validator\Password;
 
-
 use Symfony\Component\Validator\Constraint;
 
+/**
+ * @Annotation
+ */
 class Password extends Constraint
 {
     public string $message = 'Неверно введен пароль, пароль должен содержать(минимум 1 число, буквы или спец символы и иметь длину от 5 до 16)';
