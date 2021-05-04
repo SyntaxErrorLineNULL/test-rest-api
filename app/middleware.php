b67e338020366eb90509af9dc961f29add40100e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-05-04 20:45:27 +0300

    update middleware.php

--- app/middleware.php
+++ app/middleware.php
@@ -7,9 +7,11 @@
 declare(strict_types=1);
 
 use App\Api\Other\Auth\AuthenticationMiddleware;
+use App\Core\Http\Middleware\ValidationExceptionHandler;
 use Slim\App;
 
 return function (App $app) {
+    $app->add(ValidationExceptionHandler::class);
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
     $app->add(AuthenticationMiddleware::class);
